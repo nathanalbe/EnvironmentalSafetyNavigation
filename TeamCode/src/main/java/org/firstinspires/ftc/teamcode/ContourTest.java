ca48e91936969a22a295ba78737ca4c3420fc807
San68bot <san68bot@gmail.com>
2023-10-07 19:09:34 -0400

    Better Detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ContourTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ContourTest.java
@@ -34,62 +34,63 @@ public class ContourTest implements VisionProcessor {
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        // Convert the input frame to grayscale
-        Mat grayFrame = new Mat();
-        Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_RGBA2GRAY);
+        try {
+            // Convert the input frame to grayscale
+            Mat grayFrame = new Mat();
+            Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_RGBA2GRAY);
 
-        // Apply Gaussian blur with a larger kernel for noise reduction
-        if (blur != 0) Imgproc.GaussianBlur(grayFrame, grayFrame, new Size(blur, blur), 0); // Adjust kernel size as needed
+            // Apply Gaussian blur with a larger kernel for noise reduction
+            if (blur != 0) Imgproc.GaussianBlur(grayFrame, grayFrame, new Size(blur, blur), 0); // Adjust kernel size as needed
 
-        // Perform edge detection using Canny with adjusted threshold values
-        Mat edges = new Mat();
-        Imgproc.Canny(grayFrame, edges, canny_threshold1, canny_threshold2); // Adjust threshold values as needed
-        grayFrame.release();
+            // Perform edge detection using Canny with adjusted threshold values
+            Mat edges = new Mat();
+            Imgproc.Canny(grayFrame, edges, canny_threshold1, canny_threshold2); // Adjust threshold values as needed
+            grayFrame.release();
 
-        // Apply morphological operations with adjusted kernel sizes and shapes
-        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(filter_size, filter_size));
-        Imgproc.dilate(edges, edges, kernel);
-        Imgproc.erode(edges, edges, kernel);
-        kernel.release();
+            // Apply morphological operations with adjusted kernel sizes and shapes
+            Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(filter_size, filter_size));
+            Imgproc.dilate(edges, edges, kernel);
+            Imgproc.erode(edges, edges, kernel);
+            kernel.release();
 
-        // Find contours in the binary image
-        List<MatOfPoint> contours = new ArrayList<>();
-        Imgproc.findContours(edges, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            // Find contours in the binary image
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(edges, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-        // Dilate the contours to make them thicker
-        Mat dilatedEdges = new Mat();
-        Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilationSize, dilationSize));
-        Imgproc.dilate(edges, dilatedEdges, dilateKernel);
-        dilateKernel.release();
-        Core.bitwise_or(dilatedEdges, edges, dilatedEdges);
-        Core.bitwise_not(dilatedEdges, dilatedEdges);
-        edges.release();
+            // Dilate the contours to make them thicker
+            Mat dilatedEdges = new Mat();
+            Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(dilationSize, dilationSize));
+            Imgproc.dilate(edges, dilatedEdges, dilateKernel);
+            dilateKernel.release();
+            Core.bitwise_or(dilatedEdges, edges, dilatedEdges);
+            Core.bitwise_not(dilatedEdges, dilatedEdges);
+            edges.release();
 
-        // Find contours in the dilated binary image
-        List<MatOfPoint> dilatedContours = new ArrayList<>();
-        Imgproc.findContours(dilatedEdges, dilatedContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-        dilatedEdges.release();
+            // Find contours in the dilated binary image
+            List<MatOfPoint> dilatedContours = new ArrayList<>();
+            Imgproc.findContours(dilatedEdges, dilatedContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+            dilatedEdges.release();
 
-        // Draw lines around detected contours on the original frame
-        for (MatOfPoint contour : dilatedContours) {
-            double contourArea = Imgproc.contourArea(contour);
-            if (contourArea < min_area) {
-                continue;
+            // Draw lines around detected contours on the original frame
+            for (MatOfPoint contour : dilatedContours) {
+                double contourArea = Imgproc.contourArea(contour);
+                if (contourArea < min_area) {
+                    continue;
+                }
+                // Find the bounding box of the contour
+                Imgproc.fillPoly(frame, Collections.singletonList(contour), new Scalar(0, 0, 255)); // You can adjust the color here
+                // draw center of each contour
+                Moments moments = Imgproc.moments(contour);
+                Point center = new Point(moments.get_m10() / moments.get_m00(), moments.get_m01() / moments.get_m00());
+                Imgproc.circle(frame, center, 5, new Scalar(0, 255, 0), 2);
             }
-            // Find the bounding box of the contour
-            Imgproc.fillPoly(frame, Collections.singletonList(contour), new Scalar(0, 0, 255)); // You can adjust the color here
-            // draw center of each contour
-            Moments moments = Imgproc.moments(contour);
-            Point center = new Point(moments.get_m10() / moments.get_m00(), moments.get_m01() / moments.get_m00());
-            Imgproc.circle(frame, center, 5, new Scalar(0, 255, 0), 2);
+        } catch (Exception e) {
+            telemetry.addData("Exception", e.getMessage());
         }
 
         telemetry.update();
         return null;
     }
-
-
-
 
     @Override
     public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
